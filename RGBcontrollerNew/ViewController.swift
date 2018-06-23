082ea7afcbc3591308e0e8e2ebd612b4c74d6280
Екатерина Горбань <soldner166@mail.ru>
2018-06-23 16:54:03 +0300

    Initial Commit

---
+++ RGBcontrollerNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGBcontrollerNew
+//
+//  Created by Екатерина Горбань on 23.06.2018.
+//  Copyright © 2018 MIkhail Gorban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
