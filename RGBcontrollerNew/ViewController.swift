ec3123cc54cd62f93bbe976943353ec323a9d542
Екатерина Горбань <soldner166@mail.ru>
2018-06-24 18:18:16 +0300

    add basic functional

--- RGBcontrollerNew/ViewController.swift
+++ RGBcontrollerNew/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var redTextField: UITextField!
     @IBOutlet weak var greenTextField: UITextField!
     @IBOutlet weak var blueTextField: UITextField!
+    var red: Float = 0.0
+    var green: Float = 0.0
+    var blue: Float = 0.0
     
     func myViewColor (red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
         return UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1.0
@@ -41,24 +44,24 @@ class ViewController: UIViewController {
     clearButton.layer.cornerRadius = 20
     clearButton.layer.borderColor = UIColor.black.cgColor
     clearButton.layer.borderWidth = 1.0
-    redSlider.minimumValue = 0
-    redSlider.maximumValue = 256
+    redSlider.minimumValue = 0.0
+    redSlider.maximumValue = 255.0
     redSlider.tintColor = UIColor.red
-    redSlider.value = 0
-    greenSlider.minimumValue = 0
-    greenSlider.maximumValue = 256
+    redSlider.value = 0.0
+    greenSlider.minimumValue = 0.0
+    greenSlider.maximumValue = 255.0
     greenSlider.tintColor = UIColor.green
-    greenSlider.value = 0
-    blueSlider.minimumValue = 0
-    blueSlider.maximumValue = 256
+    greenSlider.value = 0.0
+    blueSlider.minimumValue = 0.0
+    blueSlider.maximumValue = 255.0
     blueSlider.tintColor = UIColor.blue
-    blueSlider.value = 0
-    redTextField.keyboardType = .numberPad
+    blueSlider.value = 0.0
     redTextField.text = "0"
     greenTextField.text = "0"
-    greenTextField.keyboardType = .numberPad
     blueTextField.text = "0"
-    blueTextField.keyboardType = .numberPad
+    red = redSlider.value
+    green = greenSlider.value
+    blue = blueSlider.value
     myView.backgroundColor = myViewColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value))
     }
 
@@ -71,19 +74,76 @@ class ViewController: UIViewController {
 
     
     @IBAction func redSliderAction(_ sender: Any) {
-
+    red = redSlider.value
+    myView.backgroundColor = myViewColor(red: CGFloat(red / 255.0), green: CGFloat(green / 255.0), blue: CGFloat(blue / 255.0))
+    redTextField.text = String(format: "%.0f", red)
     }
     @IBAction func greenSliderAction(_ sender: Any) {
+    green = greenSlider.value
+        myView.backgroundColor = myViewColor(red:CGFloat(red / 255.0), green: CGFloat(green / 255.0), blue:CGFloat(blue / 255.0))
+    greenTextField.text = String(format: "%.0f", green)
     }
     @IBAction func blueSliderAction(_ sender: Any) {
+    blue = blueSlider.value
+        myView.backgroundColor = myViewColor(red: CGFloat(red / 255.0), green: CGFloat(green / 255.0), blue: CGFloat(blue / 255.0))
+    blueTextField.text = String(format: "%.0f", blue)
     }
     @IBAction func redTxFieldAction(_ sender: Any) {
+        guard redTextField.text!.isEmpty == false else {return}
+        let redNum = Double(redTextField.text!)
+        if redNum! < 0 || redNum! > 255 {
+            let alert = UIAlertController(title: "Wrong number", message: "Code must be from 0 to 255", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            redTextField.text = "0"
+        } else {
+            red = Float(redNum!)
+            redSlider.value = red
+            myView.backgroundColor = myViewColor(red: CGFloat(red / 255.0), green: CGFloat(green / 255.0), blue: CGFloat(blue / 255.0))
+        }
     }
     @IBAction func greenTxField(_ sender: Any) {
+        guard greenTextField.text!.isEmpty == false else {return}
+        let greenNum = Double(greenTextField.text!)
+        if greenNum! < 0 || greenNum! > 255 {
+            let alert = UIAlertController(title: "Wrong number", message: "Code must be from 0 to 255", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            greenTextField.text = "0"
+        } else {
+            green = Float(greenNum!)
+            greenSlider.value = green
+            myView.backgroundColor = myViewColor(red: CGFloat(red / 255.0), green: CGFloat(green / 255.0), blue: CGFloat(blue / 255.0))
+        }
     }
     @IBAction func blueTxAction(_ sender: Any) {
+        guard blueTextField.text!.isEmpty == false else {return}
+        let blueNum = Double(blueTextField.text!)
+        if blueNum! < 0 || blueNum! > 255 {
+            let alert = UIAlertController(title: "Wrong number", message: "Code must be from 0 to 255", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            blueTextField.text = "0"
+        } else {
+            blue = Float(blueNum!)
+            blueSlider.value = red
+            myView.backgroundColor = myViewColor(red: CGFloat(red / 255.0), green: CGFloat(green / 255.0), blue: CGFloat(blue / 255.0))
+        }
     }
     @IBAction func clearButtonAction(_ sender: Any) {
+    redSlider.value = 0.0
+    greenSlider.value = 0.0
+    blueSlider.value = 0.0
+    red = redSlider.value
+    green = greenSlider.value
+    blue = blueSlider.value
+    redTextField.text = "0"
+    greenTextField.text = "0"
+    blueTextField.text = "0"
+    myView.backgroundColor = myViewColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue))
     }
     
 
